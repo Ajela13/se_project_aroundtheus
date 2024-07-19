7018c121849c14610ac0ea339dd5ed1e236bfbbe
alejandra marino <alejamarino123@gmail.com>
2024-07-19 11:48:03 -0400

    fix: add image description

--- src/components/ModalWithImage.js
+++ src/components/ModalWithImage.js
@@ -4,11 +4,14 @@ export default class ModalWithImage extends Modal {
   constructor(modalSelector) {
     super(modalSelector);
     this._previewImage = this._modal.querySelector(".modal__image");
+    this._previewImageDescription = this._modal.querySelector(
+      ".preview_modal_description"
+    );
   }
 
   open(cardData) {
     this._previewImage.src = cardData.link;
-    this._previewImage.textContent = cardData.name;
+    this._previewImageDescription.textContent = cardData.name;
     this._previewImage.alt = cardData.alt;
     super.open();
   }
