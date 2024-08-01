86e7ba0c015b4f9a74c855000d4f1b5c5ed3d0e8
alejandra marino <alejamarino123@gmail.com>
2024-08-01 13:03:26 -0400

    feat: add Api class and contructor

---
+++ src/components/Api.js
@@ -0,0 +1,9 @@
+export default class Api {
+  constructor() {
+    this.baseUrl = "https://around-api.en.tripleten-services.com/v1";
+    this.header = {
+      authorization: "e0de40de-7d64-4892-9ff7-934d3c5c1077",
+      "Content-Type": "application/json",
+    };
+  }
+}
