e0cfd241b4e49ff353fcd116dddc95ecef9e5a59
alejandra marino <alejamarino123@gmail.com>
2024-08-04 20:07:10 -0400

    feat: create modal comfirmation class with constructor

---
+++ src/components/ModalWithConfirmation.js
@@ -0,0 +1,22 @@
+import Modal from "./Modal.js";
+
+export default class ModalWithConfirmation extends Modal {
+  constructor(modalSelector) {
+    super(modalSelector);
+    this._confirmationModalForm = this._modal.querySelector(".modal__form");
+    this._confirmationButton =
+      this._confirmationModalForm.querySelector("form__button");
+  }
+
+  setSubmitAction(Action) {
+    this._submitAction = Action;
+  }
+  setEventListeners() {
+    super.setEventListeners();
+    this._confirmationModalForm.addEventListener("submit", (evt) => {
+      evt.preventDefault();
+      this._submitAction();
+      this.close();
+    });
+  }
+}
