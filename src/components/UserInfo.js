cda7695c5d11207ef478705c8da8ea5032edcf3e
alejandra marino <alejamarino123@gmail.com>
2024-08-04 20:54:49 -0400

    feat: get user Avatar inside class

--- src/components/UserInfo.js
+++ src/components/UserInfo.js
@@ -1,9 +1,14 @@
 export default class UserInfo {
-  constructor(profileTitleSelector, profileDescriptionSelector) {
+  constructor(
+    profileTitleSelector,
+    profileDescriptionSelector,
+    profileAvatarSelector
+  ) {
     this._profileTitleElement = document.querySelector(profileTitleSelector);
     this._profileDescriptionElement = document.querySelector(
       profileDescriptionSelector
     );
+    this._profileAvatarElement = document.querySelector(profileAvatarSelector);
   }
 
   getUserInfo() {
@@ -16,5 +21,9 @@ export default class UserInfo {
   setUserInfo({ title, description }) {
     this._profileTitleElement.textContent = title;
     this._profileDescriptionElement.textContent = description;
+  }
+
+  setUserAvatar({ avatar }) {
+    this._profileAvatarElement.src = avatar;
   }
 }
